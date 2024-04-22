5ded324d1a9a5a6ec7e90d87d54c5dd286020599
TheXamlGuy <dan_clark@outlook.com>
2024-04-22 22:49:04 +0100

    Added KeyBindingTriggerBehavior

---
+++ Toolkit.UI.Avalonia/KeyBindingTriggerBehavior.cs
@@ -0,0 +1,43 @@
+﻿using Avalonia;
+using Avalonia.Input;
+using Avalonia.Xaml.Interactivity;
+using System.Windows.Input;
+
+namespace Toolkit.UI.Avalonia;
+
+public class KeyBindingTriggerBehavior : 
+    Trigger<InputElement>, 
+    ICommand
+{
+    public static readonly StyledProperty<KeyGesture> GestureProperty =
+        AvaloniaProperty.Register<KeyBindingTriggerBehavior, KeyGesture>(nameof(Gesture));
+
+    public KeyGesture Gesture
+    {
+        get => GetValue(GestureProperty);
+        set => SetValue(GestureProperty, value);
+    }
+
+    public event EventHandler? CanExecuteChanged;
+
+    protected override void OnAttached()
+    {
+        if (Gesture is not null)
+        {
+            KeyBinding keyBinding = new KeyBinding
+            {
+                Gesture = Gesture,
+                Command = this
+            };
+
+            AssociatedObject?.KeyBindings.Add(keyBinding);
+        }
+
+        base.OnAttached();
+    }
+
+    public bool CanExecute(object? parameter) => true;
+
+    public void Execute(object? parameter) => 
+        Interaction.ExecuteActions(AssociatedObject, Actions, null);
+}
