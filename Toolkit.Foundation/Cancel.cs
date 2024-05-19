718a1f92ca59fe93e1dbd76d9a32b5d31e639143
TheXamlGuy <dan_clark@outlook.com>
2024-05-19 16:13:45 +0100

    Support unarchiving

---
+++ Toolkit.Foundation/Cancel.cs
@@ -0,0 +1,10 @@
+﻿namespace Toolkit.Foundation;
+
+public record Cancel
+{
+    public static CancelEventArgs<TValue> As<TValue>(TValue value) =>
+        new(value);
+
+    public static CancelEventArgs<TValue> As<TValue>() where TValue : new() =>
+        new(new TValue());
+}
