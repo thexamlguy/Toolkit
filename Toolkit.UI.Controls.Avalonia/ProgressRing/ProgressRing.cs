28763c2b32503c11797c64ebdca7863e98c6971d
TheXamlGuy <dan_clark@outlook.com>
2024-06-15 20:35:24 +0100

    Add ProgressRing Add indicator when creating a wallet

---
+++ Toolkit.UI.Controls.Avalonia/ProgressRing/ProgressRing.cs
@@ -0,0 +1,8 @@
+﻿namespace Toolkit.UI.Controls.Avalonia;
+
+public class ProgressRing :
+    FluentAvalonia.UI.Controls.ProgressRing
+{
+    protected override Type StyleKeyOverride =>
+        typeof(FluentAvalonia.UI.Controls.ProgressRing);
+}
