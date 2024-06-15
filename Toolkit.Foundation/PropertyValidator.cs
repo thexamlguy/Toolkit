bca4a32964c153330d07e6512bd397912332a03a
TheXamlGuy <dan_clark@outlook.com>
2024-06-15 22:16:03 +0100

    Validation work

---
+++ Toolkit.Foundation/PropertyValidator.cs
@@ -0,0 +1,22 @@
+﻿namespace Toolkit.Foundation;
+
+public class PropertyValidator
+{
+    public PropertyValidator(Func<bool> validation,
+        string message)
+    {
+        Validation = validation;
+        Message = new Func<string>(() => message);
+    }
+
+    public PropertyValidator(Func<bool> validation,
+        Func<string> message)
+    {
+        Validation = validation;
+        Message = message;
+    }
+
+    public Func<string> Message { get; }
+
+    public Func<bool>? Validation { get; }
+}
---
+++ Toolkit.Foundation/Validator.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Toolkit.Foundation;
+
+public class Validator
+{
+    private readonly Action? propertyChanged;
+    private readonly PropertyValidator? propertyValidation;
+
+    internal Validator(string propertyName,
+        Action propertyChanged)
+    {
+        PropertyName = propertyName;
+        this.propertyChanged = propertyChanged;
+    }
+
+    internal Validator(string propertyName,
+        Action propertyChanged,
+        PropertyValidator validation)
+    {
+        PropertyName = propertyName;
+
+        this.propertyChanged = propertyChanged;
+        propertyValidation = validation;
+    }
+
+    internal Validator(string propertyName,
+        PropertyValidator validation)
+    {
+        PropertyName = propertyName;
+        propertyValidation = validation;
+    }
+
+    public string? PropertyName { get; }
+
+    public void Set() => propertyChanged?.Invoke();
+
+    public bool TryValidate([MaybeNull] out string message)
+    {
+        message = "";
+
+        if (propertyValidation is not null && propertyValidation.Validation?.Invoke() == false)
+        {
+            message = propertyValidation.Message.Invoke();
+            return false;
+        }
+
+        propertyChanged?.Invoke();
+        return true;
+    }
+}
