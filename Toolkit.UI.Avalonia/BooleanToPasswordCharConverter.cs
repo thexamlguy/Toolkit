02c7c03251a11e19745753b93c40b4150455204a
TheXamlGuy <dan_clark@outlook.com>
2024-07-06 17:01:50 +0100

    add readonly password reveal

---
+++ Toolkit.UI.Avalonia/BooleanToPasswordCharConverter.cs
@@ -0,0 +1,21 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml;
+using System.Globalization;
+
+namespace Toolkit.UI.Avalonia;
+
+public class BooleanToPasswordCharConverter :
+    MarkupExtension,
+    IValueConverter
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) => 
+        this;
+
+    public char PasswordChar { get; set; }
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) =>
+        value is bool boolValue ? boolValue ? '\0' : PasswordChar : (object)PasswordChar;
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => 
+        throw new NotImplementedException();
+}
