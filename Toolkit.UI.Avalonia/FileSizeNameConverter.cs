ee7ac8b28bebceec3071ecd680cd6d6bcc1b4962
TheXamlGuy <dan_clark@outlook.com>
2024-07-24 22:11:36 +0100

    Added FileSizeNameConverter

---
+++ Toolkit.UI.Avalonia/FileSizeNameConverter.cs
@@ -0,0 +1,37 @@
+﻿using Avalonia.Data.Converters;
+using Avalonia.Markup.Xaml;
+using System.Globalization;
+
+namespace Toolkit.UI.Avalonia;
+
+public class FileSizeNameConverter : 
+    MarkupExtension,
+    IValueConverter
+{
+    public override object ProvideValue(IServiceProvider serviceProvider) =>
+        this;
+
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is long fileSize)
+        {
+            string[] sizeSuffixes = ["bytes", "KB", "MB", "GB", "TB", "PB"];
+
+            int order = 0;
+            double adjustedSize = fileSize;
+
+            while (adjustedSize >= 1024 && order < sizeSuffixes.Length - 1)
+            {
+                order++;
+                adjustedSize /= 1024;
+            }
+
+            return $"{adjustedSize:0.##} {sizeSuffixes[order]}";
+        }
+
+        return "0 bytes";
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) => 
+        throw new NotImplementedException();
+}
