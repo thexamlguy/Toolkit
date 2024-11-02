469a52efaa9bb78153b0c02770cfd70fd362b3bf
Dan Clark <dan.clark@videosoftglobal.com>
2024-11-02 21:33:17 +0000

    Update mnigets

---
+++ Toolkit.WinUI/IServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Toolkit.Foundation;
+
+namespace Toolkit.WinUI;
+
+public static class IServiceCollectionExtensions
+{
+    public static IServiceCollection AddWinUI(this IServiceCollection services)
+    {
+        services.AddTransient<IDispatcherTimerFactory, DispatcherTimerFactory>();
+        return services;
+    }
+}
