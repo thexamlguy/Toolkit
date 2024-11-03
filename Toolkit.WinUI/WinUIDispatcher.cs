e53100de133f84e75a686a6be363c51877cb60e1
Dan Clark <dan.clark@videosoftglobal.com>
2024-11-03 14:12:29 +0000

    Add WinUIDispatcher

---
+++ Toolkit.WinUI/WinUIDispatcher.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.UI.Dispatching;
+using Toolkit.Foundation;
+
+namespace Toolkit.WinUI;
+
+public class WinUIDispatcher :
+    IDispatcher
+{
+    public Task Invoke(Action action)
+    {
+        DispatcherQueue.GetForCurrentThread().TryEnqueue(action.Invoke);
+        return Task.CompletedTask;
+    }
+}
